7275528626a661701bee087e9b3d1b123f570e3d
loki <jw.park@fromthered.com>
2020-05-22 17:50:06 +0900

    upload file - 진행 중

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,7 +4,7 @@ import { validateFirebaseIdToken } from './_common';
 import UserController from '../controllers/userController';
 import AlarmController from "../controllers/alarmController";
 import RpcController from '../controllers/rpcController';
-import FileManager from '../services/fileManager';
+// import FileManager from '../services/fileManager';
 
 
 const apiVer = `/api/v1`;
@@ -15,7 +15,7 @@ export default (router: Router) => {
 
     router.get(`${apiVer}/users/info`,              validateFirebaseIdToken,    convert(UserController.getInfo));
     router.get(`${apiVer}/users/info/:target_uid`,  validateFirebaseIdToken,    convert(UserController.getTargetInfo));
-    router.post(`${apiVer}/user/u`,             validateFirebaseIdToken,    FileManager.uploadImage, convert(UserController.setInfo));
+    // router.post(`${apiVer}/user/u`,             validateFirebaseIdToken,    FileManager.uploadImage, convert(UserController.setInfo));
 
     router.post(`${apiVer}/user/setting`,       validateFirebaseIdToken,    convert(UserController.updateSetting));
     router.get(`${apiVer}/user/alarm`,          validateFirebaseIdToken,    convert(AlarmController.getList));
