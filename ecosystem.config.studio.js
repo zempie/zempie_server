de2b3b29fdd4f0ce1486584c64278d99026f377a
hg.park <hg.park@fromthered.com>
2020-08-28 18:46:07 +0900

    스튜디오 에코시스템 추가

---
+++ ecosystem.config.studio.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+
+module.exports = {
+    apps: [
+        {
+            name: 'platform-server-studio',
+            script: './dist/src/runStudio.js',
+            watch: false,
+            ignore_watch : ["node_modules", "public"],
+            env: {
+                NODE_ENV: 'development'
+            },
+            env_production: {
+                NODE_ENV: 'production'
+            },
+            env_staging: {
+                NODE_ENV: 'staging'
+            }
+        }
+    ],
+
+    deploy: {
+        development: {
+            user: 'loki',
+            host: [{host:'192.168.0.10', port:'2105'}],
+            ref : 'origin/dev_studio',
+            repo: 'git@192.168.0.36:raptor/platform-api-server.git',
+            path: '/home/loki/project/raptor/platform-server-studio',
+            ssh_options: '',
+            'post-deploy': 'yarn install && tsc && pm2 startOrRestart ecosystem.config.studio.js --env development'
+        },
+        staging: {
+            user: 'loki',
+            host: [{host:'192.168.0.10', port:'2105'}],
+            ref : 'origin/master',
+            repo: 'git@192.168.0.36:raptor/platform-api-server.git',
+            path: '/home/loki/project/raptor/platform-server',
+            ssh_options: '',
+            'post-deploy': 'yarn install && tsc && pm2 startOrRestart ecosystem.config.js --env staging && pm2 startOrRestart admin_ecosystem.config.js --env staging'
+        }
+    }
+};
