107e4c5e2c7d678d15095bb0e75a040eef684846
loki <jw.park@fromthered.com>
2020-10-27 18:30:06 +0900

    ...

--- src/servers/adminServer.ts
+++ src/servers/adminServer.ts
@@ -1,8 +1,16 @@
 import Server from "./server";
 import {Router} from "express";
 import adminRoute from '../routes/adminRoute';
+import { IServerOptions } from '../commons/interfaces';
 
 class AdminServer extends Server {
+    initialize = async (options: IServerOptions) => {
+        this.options = options;
+
+        this.setExpress(options);
+        await AdminServer.setRDB();
+    }
+
     routes(app: Router) {
         super.routes(app);
 
