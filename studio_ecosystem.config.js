7c61c0f18ef615cbab42fb91bef860e8be3ba5c6
loki <jw.park@fromthered.com>
2020-11-27 17:22:01 +0900

    dist

--- studio_ecosystem.config.js
+++ studio_ecosystem.config.js
@@ -5,7 +5,7 @@
 module.exports = {
     apps: [
         {
-            name: 'zempie-studio-server',
+            name: 'zempie-studio',
             script: './dist/src/runStudio.js',
             watch: false,
             ignore_watch : ["node_modules", "public"],
