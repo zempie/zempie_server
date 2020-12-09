4f5e3bc000b6b81f6a4f891d35dd2c343043c327
loki <jw.park@fromthered.com>
2020-12-09 19:15:09 +0900

    caches.sharedGame

--- dist/src/commons/globals.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.caches = exports.service = exports.firebase = exports.dbs = void 0;
-exports.dbs = {};
-exports.firebase = {};
-exports.service = {};
-exports.caches = {};
-//# sourceMappingURL=globals.js.map
