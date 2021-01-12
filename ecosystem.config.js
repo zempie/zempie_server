68621f9cba3a99856512904f094f897b6a59a217
loki <jw.park@fromthered.com>
2021-01-12 17:34:30 +0900

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
     deploy: {
         development: {
-            user: 'loki',
+            user: 'dev103',
             host: [{host:'192.168.0.10', port:'2105'}],
             ref : 'origin/master',
             repo: 'git@192.168.0.36:raptor/platform-api-server.git',
