f862cde650cc87e93f1db4d99212f6254392a763
loki <jw.park@fromthered.com>
2021-03-05 15:01:32 +0900

    redirect controller

--- src/controllers/redirectController.ts
+++ src/controllers/redirectController.ts
@@ -6,10 +6,30 @@ import { getGameData } from './_common';
 
 
 class RedirectController {
+    private checkUserAgent = (req: Request) => {
+        const userAgent: any = req.headers['User-Agent'.toLowerCase()];
+        return userAgent?.match(/GuzzleHttp|baiduspider|twitterbot|facebookexternalhit|rogerbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest|slackbot|vkShare|W3C_Validator/i)
+    }
+
+    private responseGame = (res: Response, game: any, url: string) => {
+        res.status(200).set('Content-Type', 'text/html')
+            .end(`<html>
+                                <head>
+                                    <title>${game.title}</title>
+                                    <meta name="description" content="${game.description}">
+                                    <meta property="og:url" content="${url}">
+                                    <meta property="og:site_name" content="Zempie">
+                                    <meta property="og:title" content="${game.title}">
+                                    <meta property="og:description" content="${game.description}">
+                                    <meta property="og:image" content="${game.url_thumb}">
+                                    <meta property="og:type" content="website">
+                                </head>
+                            </html>`);
+    }
+
     play = async (req: Request, res: Response) => {
         const { pathname } = req.params;
-        const userAgent: any = req.headers['User-Agent'.toLowerCase()];
-        if (userAgent?.match(/GuzzleHttp|baiduspider|twitterbot|facebookexternalhit|rogerbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest|slackbot|vkShare|W3C_Validator/i)) {
+        if ( this.checkUserAgent(req) ) {
             let game = caches.game.getByPathname(pathname);
             if ( !game ) {
                 game = await dbs.Game.findOne({ pathname: pathname });
@@ -18,26 +38,45 @@ class RedirectController {
                 res.redirect(`https://zempie.com`);
             }
             else {
-                game = getGameData(game)
+                game = getGameData(game);
                 caches.game.setByPathname(game, pathname);
-                res.status(200).set('Content-Type', 'text/html')
-                    .end(`<html>
-                                <head>
-                                    <title>${game.title}</title>
-                                    <meta name="description" content="${game.description}">
-                                    <meta property="og:url" content="https://zempie.com/play/${req.params.pathname}">
-                                    <meta property="og:site_name" content="Zempie">
-                                    <meta property="og:title" content="${game.title}">
-                                    <meta property="og:description" content="${game.description}">
-                                    <meta property="og:image" content="${game.url_thumb}">
-                                    <meta property="og:type" content="website">
-                                </head>
-                            </html>`);
+                this.responseGame(res, game, `https://zempie.com/play/${req.params.pathname}`);
             }
         }
         else {
             res.redirect(`https://zempie.com/redirect/play/${req.params.pathname}`)
         }
+    }
+
+
+    battle = async (req: Request, res: Response) => {
+        const { battle_uid } = req.params;
+        if ( this.checkUserAgent(req) ) {
+            let battle = caches.battle.getByUid(battle_uid);
+            if ( !battle ) {
+                battle = await dbs.Battle.model.findOne({
+                    where: { uid: battle_uid },
+                    include: [{
+                        model: dbs.Game.model,
+                    }]
+                });
+            }
+            if ( !battle ) {
+                res.redirect(`https://zempie.com`);
+            }
+            else {
+                caches.game.setByUid(battle, battle_uid);
+                const game = getGameData(battle.game)
+                this.responseGame(res, game, `https://zempie.com/battle/${req.params.battle_uid}`);
+            }
+        }
+        else {
+            res.redirect(`https://zempie.com/redirect/battle/${req.params.pathname}`)
+        }
+    }
+
+    shared = async (req: Request, res: Response) => {
+
     }
 }
 
--- src/database/redis/models/battle.ts
+++ src/database/redis/models/battle.ts
@@ -2,8 +2,23 @@ import * as _ from 'lodash';
 import CacheModel from './_cache';
 
 
+const RedirectKey = `zempie:redirect:battle:`;
+
 class BattleCache extends CacheModel {
     public readonly name = 'battle';
+
+    async getByUid(uid: string) {
+        const battle = await this.redis.get(RedirectKey + uid);
+        if ( battle ) {
+            return JSON.parse(battle)
+        }
+        return battle
+    }
+    setByUid(ret: any, uid: string) {
+        this.redis.set(RedirectKey + uid, JSON.stringify(ret), () => {
+            this.redis.expire(RedirectKey + uid, 60 * 10, () => {});
+        })
+    }
 
     async gameStart (battle_uid: string, battle_key: string, user_uid: string, best_score?: number) {
         this.redis.hset(`zempie:battle:${battle_uid}:users`, user_uid, JSON.stringify({
