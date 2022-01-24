8465e41b76cc0b4ee3496d8f98af70617e1aef13
DESKTOP-CTK4VV8\ftred_dev08 <hj.lee@fromthered.com>
2022-01-24 16:13:35 +0900

    리디렉션수정

--- src/controllers/redirectController.ts
+++ src/controllers/redirectController.ts
@@ -144,7 +144,7 @@ class RedirectController {
     }
 
     user = async (req: Request, res: Response) => {
-        res.redirect(`${Url.Redirect}/user/${req.params.community_id}`)
+        res.redirect(`${Url.Redirect}/user/${req.params.user_id}`)
     }
 
 }
