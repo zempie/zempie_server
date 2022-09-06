555506a1d5217f637be0c87ec489fb59836ce682
hyeonjeong <hj.lee@fromthered.com>
2022-09-06 11:17:53 +0900

    add/translate

--- src/routes/communityRoute.ts
+++ src/routes/communityRoute.ts
@@ -1,16 +1,19 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import convert from '../controllers/_convert';
 import CommunityController from '../controllers/communityController';
-import {validateFirebaseIdToken} from "./_common";
+import GoogleController from '../controllers/googleController'
+import { validateFirebaseIdToken } from "./_common";
 
 const apiVer = `/api/v1`;
 
 export default (router: Router) => {
 
     //커뮤니티
-    router.get(`${apiVer}/community/project/:id`,          validateFirebaseIdToken,     convert(CommunityController.getProject));
-    router.get(`${apiVer}/community/channel/:channel_id`,  validateFirebaseIdToken,     convert(CommunityController.getTargetInfoByChannelId));
-    router.get(`${apiVer}/community/user/:user_id`,        validateFirebaseIdToken,     convert(CommunityController.getTargetInfoByUserId));
+    router.get(`${apiVer}/community/project/:id`, validateFirebaseIdToken, convert(CommunityController.getProject));
+    router.get(`${apiVer}/community/channel/:channel_id`, validateFirebaseIdToken, convert(CommunityController.getTargetInfoByChannelId));
+    router.get(`${apiVer}/community/user/:user_id`, validateFirebaseIdToken, convert(CommunityController.getTargetInfoByUserId));
+
+    router.post(`${apiVer}/translate`, convert(GoogleController.translateText))
 
 
 
