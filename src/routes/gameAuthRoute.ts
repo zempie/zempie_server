ac6d6db57dc45efadcbad3c905fb80ae60f387fc
hyeonjeong <hj.lee@fromthered.com>
2022-10-19 12:49:23 +0900

    token 시간 수정

--- src/routes/gameAuthRoute.ts
+++ src/routes/gameAuthRoute.ts
@@ -9,11 +9,13 @@ const apiVer = `/api/v1`;
 export default (router: Router) => {
 
   //게임 토큰
-  router.post(`${apiVer}/create/token`, convert(gameAuthController.createToken));
+  router.post(`${apiVer}/create/token`, convert(gameAuthController.createUserToken));
   router.post(`${apiVer}/verify/token`, convert(gameAuthController.verifyToken));
 
+  //게임 서버 
   router.get(`${apiVer}/game/user/info`, validateGameToken, convert(gameAuthController.getInfo))
 
+  router.get(`${apiVer}/game/access-token`, convert(gameAuthController.createGameToken))
 
 
 }
