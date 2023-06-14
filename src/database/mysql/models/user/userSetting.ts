81a65702ac28e96007cefdf782938d5c49cd8779
hyeonjeong <hj.lee@fromthered.com>
2023-06-14 12:13:02 +0900

    채팅알람 추가

--- src/database/mysql/models/user/userSetting.ts
+++ src/database/mysql/models/user/userSetting.ts
@@ -18,12 +18,24 @@ class UserSetting extends Model {
             // notify_follow:      { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
             notify_like:        { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
             notify_reply:       { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
+            notify_chat:        { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
         }
     }
 
     async afterSync(): Promise<void> {
         this.model.belongsTo(dbs.User.model, { foreignKey: 'user_id', targetKey: 'id' });
+
+        const desc = await this.model.sequelize.queryInterface.describeTable(this.model.tableName);
+
+        if (!desc['notify_chat']) {
+            this.model.sequelize.queryInterface.addColumn(this.model.tableName, 'notify_chat', {
+                type: DataTypes.BOOLEAN,
+                after: 'notify_reply'
+            })
+        }
     }
+
+   
 }
 
 export default (rdb: Sequelize) => new UserSetting(rdb);
